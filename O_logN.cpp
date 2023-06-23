b9f39031df48a81a65d2875d62dd17039b1e953c
shakil4611 <shakilbreakingchemistry@gmail.com>
2023-06-23 15:41:38 +0600

    first commit

---
+++ O_logN.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    // while(n>0)
+    // {
+    //     int digit =n%10;
+    //     cout<<digit<<endl;
+    //     n/=10;
+    // }
+    for(int i=0; i<=n; i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
